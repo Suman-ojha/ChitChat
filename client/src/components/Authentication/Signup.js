0d49f05f0c17507bcb5a1742ef583325f2b71008
Suman-ojha <sumanojha1999@gmail.com>
2024-03-10 02:35:51 +0530

    project for dev phase shift to new repo

---
+++ client/src/components/Authentication/Signup.js
@@ -0,0 +1,194 @@
+import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { useToast } from '@chakra-ui/react'
+import axios from 'axios';
+import { useNavigate } from "react-router-dom"
+
+const Signup = () => {
+    const [show, setShow] = useState(false);
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [confirmPassword, setConfirmPassword] = useState();
+    const [pics, setPic] = useState();
+    const [loading, setLoading] = useState(false);
+    const toast = useToast();
+    const navigate = useNavigate();
+
+ 
+
+    const postDetails = async (file) => {
+        setLoading(true);
+        if (!file) {
+            toast({
+                title: "Please Select an Image!",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+            return;
+        }
+        console.log("File-->",file);
+        if (file.type === "image/jpeg" || file.type === "image/png") {
+            const data = new FormData();
+            data.append("file", file);
+            data.append("upload_preset", "chat-app");
+            data.append("cloud_name", "suman099");
+    
+            try {
+                const response = await fetch("https://api.cloudinary.com/v1_1/suman099/image/upload", {
+                    method: "POST",
+                    mode: "cors",
+                    body: data,
+                });
+    
+                if (!response.ok) {
+                    throw new Error("Failed to upload image");
+                }
+    
+                const responseData = await response.json();
+                // console.log("urlData=====>", responseData.url)
+                setPic(responseData.url.toString());
+                setLoading(false);
+            } catch (error) {
+                console.error("Error uploading image:", error);
+                setLoading(false);
+            }
+            console.log("picData===>",pics)
+        } else {
+            toast({
+                title: "Please Select an Image!",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+            return;
+        }
+    };
+    
+
+    const submitHandler = async () => {
+        setLoading(true);
+        if (!name || !email || !password || !confirmPassword) {
+            toast({
+                title: "Please fill all fields",
+                status: "warning",
+                deration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+            return;
+        }
+        if (password !== confirmPassword) {
+            toast({
+                title: "Password should be same",
+                status: "warning",
+                deration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            return;
+        }
+        try {
+            const config = {
+                headers: {
+                    "Content-type": "application/json",
+                },
+            };
+            console.log("picinsubmit====>",pics)
+            const { data } = await axios.post("api/user", { name, email, password, pics }, config);
+            toast({
+                title: "Registratoin Successfull",
+                status: "success",
+                deration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+
+            localStorage.setItem("userInfo", JSON.stringify(data));
+            setLoading(false);
+            navigate('/chats');
+        } catch (error) {
+            toast({
+                title: "Error Occured",
+                description: error.response.data.message,
+                status: "error",
+                deration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+        }
+    }
+    return (
+        <VStack spacing={"5px"}>
+            <FormControl id="first-name" isRequired>
+                <FormLabel>Name</FormLabel>
+                <Input
+                    placeholder='Enter Your Name'
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </FormControl>
+            <FormControl id="email" isRequired>
+                <FormLabel>Email</FormLabel>
+                <Input
+                    placeholder='Enter Your Email id'
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </FormControl>
+            <FormControl id="password" isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                    <Input
+                        type={show ? "text" : "password"}
+                        placeholder='Enter Your Password'
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <InputRightElement width={"4.5re"}>
+                        <Button height={"1.75rem"} size={"sm"} onClick={() => (setShow(!show))}>
+                            {show ? "Hide" : "Show"}
+                        </Button>
+                    </InputRightElement>
+                </InputGroup>
+            </FormControl>
+            <FormControl id="confirnPassword" isRequired>
+                <FormLabel>Confirn Password</FormLabel>
+                <InputGroup>
+                    <Input
+                        type={show ? "text" : "password"}
+                        placeholder='Confirm Your Password'
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                    <InputRightElement width={"4.5re"}>
+                        <Button height={"1.75rem"} size={"sm"} onClick={() => (setShow(!show))}>
+                            {show ? "Hide" : "Show"}
+                        </Button>
+                    </InputRightElement>
+                </InputGroup>
+            </FormControl>
+            <FormControl id="pic">
+                <FormLabel>Upload your pic</FormLabel>
+                <Input
+                    type="file"
+                    p={1.5}
+                    accept="image/*"
+                    onChange={(e) => postDetails(e.target.files[0])}
+                />
+            </FormControl>
+            <Button
+                colorScheme={"blue"}
+                width="100%"
+                style={{ marginTop: "35px" }}
+                onClick={submitHandler}
+                isLoading={loading}
+            >Sign Up</Button>
+        </VStack>
+    )
+}
+
+export default Signup
---
+++ server/controllers/userController.js
@@ -0,0 +1,80 @@
+const asyncHandler = require('express-async-handler');
+const generateToken = require('../config/generteToken');
+const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password, pics } = req.body;
+    // console.log(req.body)
+    if (!name || !email || !password) {
+        res.status(400).send();
+        throw new Error('Enter all the fields');
+    }
+
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+        res.status(400).send();
+        throw new Error('User already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
+
+    const user = await User.create({ name, email, password: hashedPassword, pic : pics }); // Save hashed password
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            pic: user.pic,
+            token: generateToken(user._id)
+        });
+    } else {
+        res.status(400);
+        throw new Error('Failed to create the user');
+    }
+});
+
+
+const authUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            pic: user.pic,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(401);
+        throw new Error("Invalid Email or Password");
+    }
+});
+
+
+//  /api/user?search=suman
+const allUsers = asyncHandler(async (req, res) => {
+    const searchQuery = req.query.search ? {
+        $or: [
+            { name: { $regex: req.query.search, $options: "i" } },
+            { email: { $regex: req.query.search, $options: "i" } },
+        ],
+    } : {};
+
+    const users = await User.find({
+        $and: [
+            searchQuery,
+            { _id: { $ne: req.user._id } }
+        ]
+    });
+    res.status(200).send(users);
+})
+
+
+
+module.exports = { registerUser, authUser, allUsers }
